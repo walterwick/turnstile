1baf7aa4e4fae68257af3acc22271196f0940932
walter wick <83695885+walterwick@users.noreply.github.com>
2024-11-19 15:18:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ async function verifyTurnstile(token) {
     }
     return data.success;
   } catch (error) {
-    console.error('Turnstile verification error:', error);
+    console.error('Turnstile verification errorr:', error);
     return false;
   }
 }
